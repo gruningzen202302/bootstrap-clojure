c13b46d11c180853d56f391a2cbcb1bdba6cf4f4
gruningzen202302 <gruningzen202302@gmail.com>
2023-04-15 20:03:42 -0300

    addded folder to hello  world code

---
+++ time/hello.clj
@@ -0,0 +1,8 @@
+(ns hello
+  (:require [clojure.java.io :as io]
+            [clojure.string :as s]
+            [clj-time.core :as t]
+            [clj-time.format :as f]))
+
+(defn -main []
+  (* 2 3))
